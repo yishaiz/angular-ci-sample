15e0575ed26c2f3d9e8a5794282959ddb332ef85
yishai <yishai5555@gmail.com>
2018-08-02 18:52:26 +0300

    fix test + remove prod build

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,7 +16,7 @@ describe('AppComponent', () => {
   it(`should have as title 'app'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).not.toEqual('angular-ci-sample');
+    expect(app.title).toEqual('angular-ci-sample');
   }));
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
